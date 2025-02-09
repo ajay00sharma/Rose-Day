5b457de2575600bf426b5b2e0fe4d314ecb97a9a
Ajay Sharma <112599886+ajay00sharma@users.noreply.github.com>
2025-02-09 12:23:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let path = window.location.pathname;
 
-let name = path.substring(1);
+let name = path.substring(path.lastIndexOf("/") + 1);
 
-if (name) {
+if (name && name !== "https://ajay00sharma.github.io/Rose-Day/") {
   name = name.charAt(0).toUpperCase() + name.slice(1);
   document.getElementById("nametag").innerText = "By Your one and only " + name + "✨";
 } else {
